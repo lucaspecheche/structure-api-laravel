7b562dd768bb13ccc6b1b722194cd593835adc4c
Lucas Lima <lucaspecheche@gmail.com>
2019-03-02 23:08:40 -0300

    Created migrations Role, permissions and Relationships

---
+++ database/migrations/2019_03_03_003929_create_permission_roles_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermissionRolesTable extends Migration
+{
+
+    public function up()
+    {
+        Schema::create('permission_roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('permission_id')->unsigned();
+            $table->integer('role_id')->unsigned();
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('permission_roles');
+    }
+}
