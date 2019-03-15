1a637a16e44d883b6fc8669e1608e05947c1b6fc
Lucas Lima <lucaspecheche@gmail.com>
2019-03-15 09:43:11 -0300

    policy permissions

---
+++ app/Domain/Enumerators/UserPermissions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Enumerators;
+
+
+final class UserPermissions
+{
+    const NAME = "USER";
+
+    const CREATE = self::NAME.'.CREATE';
+    const EDIT   = self::NAME.'.EDIT';
+    const SHOW   = self::NAME.'.SHOW';
+}
